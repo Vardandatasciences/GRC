839d9f93b614b72e936e610d2ca38fea949ea161
Vardandatasciences <vdatasciences@gmail.com>
2025-05-26 16:29:39 +0530

    Update cypress.config.js

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -2,11 +2,8 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:5000', // Your frontend serve URL
+    baseUrl: 'http://localhost:5000',
     specPattern: 'cypress/e2e/**/*.cy.js',
     supportFile: 'cypress/support/e2e.js',
-    setupNodeEvents(on, config) {
-      // implement node event listeners here if needed
-    },
   },
 })
